4901446cad1504be5cf8d9bc67414e47523803cc
Slashome <mescomptes.flow@gmail.com>
2018-07-09 00:12:42 +0200

    First workable version release

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"github.com/slashome/script-launcher/print"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/slashome/script-launcher/print"
 )
 
 const version string = "0.0.1"
@@ -13,7 +17,7 @@ func main() {
 	args := os.Args[1:]
 	// test if scripts path as been given
 	if len(args) == 0 {
-		print.Error("Please entrer the scripts path as a first argument", false)
+		print.Error("Please entrer the scripts path as a first argument")
 		os.Exit(0)
 	}
 	// get script path
@@ -21,14 +25,41 @@ func main() {
 	// test if folder exists
 	_, err := os.Stat(path)
 	if err != nil {
-		print.Error("Folder "+path+" does not exist", false)
+		print.Error("Folder " + path + " does not exist")
 		os.Exit(0)
 	}
 	// test if options have been given
 	if len(args) < 2 {
-		print.List(path)
+		print.List(path, "")
 		os.Exit(0)
 	}
-	// group := args[1]
-	// action := args[2]
+	optionsStr := args[1]
+	options := strings.Split(optionsStr, ":")
+	group := options[0]
+	dir, err := os.Open(path + "/" + group)
+	if err != nil {
+		print.ErrorWithUsageAndList("Unable to find group "+"\""+group+"\"", path, "")
+		os.Exit(0)
+	}
+	defer dir.Close()
+
+	if len(options) < 2 {
+		print.List(path, group)
+		os.Exit(0)
+	}
+
+	command := options[1]
+	file, err := os.Open(path + "/" + group + "/" + command)
+	if err != nil {
+		print.ErrorWithUsageAndList("Unable to find command "+"\""+command+"\""+" for group "+"\""+group+"\"", path, group)
+		os.Exit(0)
+		os.Exit(0)
+	}
+	defer file.Close()
+
+	cmd := exec.Command(path + "/" + group + "/" + command)
+	fmt.Println("Running command and waiting for it to finish...")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
--- print/print.go
+++ print/print.go
@@ -1,66 +1,136 @@
 package print
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
+	"unicode/utf8"
+
 	"github.com/fatih/color"
 )
 
-var version string = "0";
+var version = "0"
+var commentMargin int = 4
 
+// SetVersion set the application version to be printed (default 0)
 func SetVersion(v string) {
 	version = v
 }
 
-func Error(message string, listPath bool) {
-	Header()
+// Error print the error message
+func Error(message string) {
+	printHeader()
 	errorMessage(message)
-	Usage()
 }
 
-func errorMessage(message string) {
-	fmt.Println(color.RedString(message))
+// Error print the error message and usage
+func ErrorWithUsage(message string) {
+	Error(message)
+	printUsage()
 }
 
-func Header() {
-	fmt.Println("Script Launcher", color.GreenString(version))
+// Error print the error message, usage and list
+func ErrorWithUsageAndList(message string, path string, group string) {
+	Error(message)
+	printUsage()
+	printList(path, group)
 }
 
-func Usage() {
-	fmt.Println("")
-	fmt.Println(color.YellowString("Usage:"))
-	fmt.Println("  command [path] [group:command|option] [arguments]")
+// List print the list of available commands
+// If group paramater is not empty string,
+// it will print only the commands this group
+func List(path string, group string) {
+	printHeader()
+	printList(path, group)
 }
 
-func List(path string) {
-	Header()
-	Usage()
+func printList(path string, group string) {
 	fmt.Println("")
 	fmt.Println(color.YellowString("Available commands:"))
 	dirs, err := ioutil.ReadDir(path)
 	if err != nil {
 		errorMessage("Impossible to read directory")
 	}
+	// Iterate one time throught dir and file to check data and count longest
+	// comment space
+	var errors []string
+	var textMaxLength int
 	for _, d := range dirs {
 		if !d.IsDir() {
-			errorMessage(d.Name() + " is not a directory. Please move it in a group.")
-			os.Exit(0)
+			errors = append(errors, "\""+d.Name()+"\" is not a directory. Please move it in a group.")
+			break
+		}
+		files, err := ioutil.ReadDir(path + "/" + d.Name())
+		if err != nil {
+			errors = append(errors, "Impossible to read directory \""+d.Name()+"\"")
+		}
+		for _, f := range files {
+			if f.IsDir() {
+				errors = append(errors, "\""+f.Name()+"\" is a directory. Please move it to group list.")
+				break
+			}
+			textLength := utf8.RuneCountInString(d.Name()+":"+f.Name()) + commentMargin
+			if textLength > textMaxLength {
+				textMaxLength = textLength
+			}
 		}
 	}
 
 	for _, d := range dirs {
-		fmt.Println(color.YellowString(d.Name()))
-		files, err := ioutil.ReadDir(path+"/"+d.Name())
-		if err != nil {
-			errorMessage("Impossible to read directory")
-		}
-		for _, f := range files {
-			if f.IsDir() {
-				errorMessage(f.Name() + " is a directory. Please move it to group list.")
-				os.Exit(0)
+		if group != "" {
+			if d.Name() != group {
+				continue
 			}
-			fmt.Printf(" %s:%s\n", color.GreenString(d.Name()), color.GreenString(f.Name()))
+		}
+		fmt.Println(color.YellowString(d.Name()))
+		files, _ := ioutil.ReadDir(path + "/" + d.Name())
+		for _, f := range files {
+			filepath := path + "/" + d.Name() + "/" + f.Name()
+			textLength := utf8.RuneCountInString(d.Name() + ":" + f.Name())
+			comment := getComment(filepath)
+			fmt.Printf(" %s:%s%s%s\n", color.GreenString(d.Name()), color.GreenString(f.Name()), getNSpaces(textMaxLength-textLength), comment)
 		}
 	}
+}
+
+func printHeader() {
+	fmt.Println("Script Launcher", color.GreenString(version))
+}
+
+func getNSpaces(n int) string {
+	var spaces string
+	for i := 0; i < n; i++ {
+		spaces = spaces + " "
+	}
+	return spaces
+}
+
+func printUsage() {
+	fmt.Println("")
+	fmt.Println(color.YellowString("Usage:"))
+	fmt.Println("  command [path] [group:command|option] [arguments]")
+}
+
+func getComment(path string) string {
+	fileHandle, _ := os.Open(path)
+	defer fileHandle.Close()
+	fileScanner := bufio.NewScanner(fileHandle)
+
+	var count int
+	for fileScanner.Scan() {
+		count++
+		if strings.HasPrefix(fileScanner.Text(), "# ") {
+			return strings.TrimPrefix(fileScanner.Text(), "# ")
+		}
+		if count == 2 {
+			break
+		}
+	}
+	return "No comment"
+}
+
+func errorMessage(message string) {
+	fmt.Println(color.RedString(message))
 }
